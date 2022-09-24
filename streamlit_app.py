a60b89b7bcd2c8f48878dcb6741baa97c5ef7cb8
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-09-23 21:30:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,31 @@
-#criar hello word no streamlit
+#criar nuvens de palavras com base em um texto digitado
 import streamlit as st
-st.title('Hello World')
+import pandas as pd
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+st.title('Word Cloud Generator')
+
+st.markdown('''
+This app generates a word cloud from a text input.
+''')
+
+st.sidebar.header('User Input Features')
+
+def get_input():
+    '''Retorna o texto digitado pelo usuário'''
+    return st.sidebar.text_area('Text Input')
+
+def get_wordcloud(text):
+    '''Retorna a nuvem de palavras'''
+    wordcloud = WordCloud().generate(text)
+    return wordcloud.to_image()
+
+text = get_input()
+st.subheader('Word Cloud')
+st.image(get_wordcloud(text))
+
+st.markdown('''Retorna o texto digitado pelo usuário''')
+
+st.markd
