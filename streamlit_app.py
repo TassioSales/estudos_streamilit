6cb12b15dc654bf690b617786c76d4e41c8d7f25
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-09-23 21:33:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,16 @@ import pandas as pd
 
 #criar titulo
 st.title('Questionário de avaliação de desempenho')
+
+#criar subtitulo
+st.subheader('Avalie o desempenho do funcionário')
+
+#criar um texto
+st.text('Avalie o desempenho do funcionário')
+
+
+#criar um checkbox
+st.checkbox('Avaliar')
+
+#criar um radio
+st.radio('Avaliar', ('Sim', 'Não'))
